e462f8db5be94202a1841bd9372268a7ae1e435e
Andrew Wilkins <afwilkin>
2020-03-28 23:54:04 -0500

    added video streaming port

--- constants.h
+++ constants.h
@@ -7,5 +7,6 @@ namespace constants{
     static const std::string from_worker = "5557";
     static const std::string to_drone = "5556";
     static const std::string from_drone = "5555";
+    static const std::string video_port = "5000";
 
 }
