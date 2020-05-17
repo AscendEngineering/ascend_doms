320cf1ff29641325cb6861ac48636d1ee46358a9
Andrew Wilkins <afwilkin>
2020-05-17 15:11:08 -0500

    moved logging outside of shared files

--- utilities.cpp
+++ utilities.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <iostream>
 #include <chrono>
-#include "spdlog/async.h"
+
 
 
 bool is_ip(const std::string& url){
@@ -29,26 +29,6 @@ std::string utilities::resolveDNS(const std::string& url){
     }
 
     return retval;
-}
-
-
-void utilities::setup_logging(){
-
-    spdlog::init_thread_pool(8192, 1);
-    
-    //console logger
-    auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    
-    //file logger
-    auto max_size = 1048576 * 5;
-    auto max_files = 3;
-    std::string filename = "logs/" + std::to_string(now_epoch()) + ".log";
-    auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>( filename , max_size, max_files);
-    
-    std::vector<spdlog::sink_ptr> sinks {console_sink, file_sink};
-    auto logger = std::make_shared<spdlog::async_logger>("base", sinks.begin(), sinks.end(), spdlog::thread_pool(), spdlog::async_overflow_policy::block);
-    spdlog::set_default_logger(logger);
-
 }
 
 
--- utilities.h
+++ utilities.h
@@ -5,15 +5,11 @@
 #include <netdb.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "spdlog/spdlog.h"
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/sinks/rotating_file_sink.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
+
 
 
 namespace utilities{
     std::string resolveDNS(const std::string& url);
-    void setup_logging();
     long now_epoch();
 }
 
